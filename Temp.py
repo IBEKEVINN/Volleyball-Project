91fd3ec939e79fe5fe81886c22b5909756d62159
Kevin Hai <90134153+IBEKEVINN@users.noreply.github.com>
2022-01-04 19:22:07 +1100

    Initial commit

---
+++ Temp.py
@@ -0,0 +1,6 @@
+columns = ['Shirt Number', 'Player Name', 'Team', 'Apts', 'Bpts', 'Spts', 'TotPts']
+vnl2021.columns = columns
+
+for player in vnl2021['Player Name']:
+    print(player)
+
---
+++ Volleyball Stats/Volleyball Stats.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import requests
+
+vnl2021 = pd.read_csv("VNL2021.csv", encoding='utf-8')
+print(vnl2021)
+
